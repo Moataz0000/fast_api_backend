b40c107f3442a18423c839bf9fe9dcda98f57582
Moataz0000 <moatazfawzy150@gmail.com>
2026-03-12 17:08:43 +0200

    feat: created service layer

--- src/domain/service/create_task.py
+++ src/domain/service/create_task.py
@@ -1,4 +1,14 @@
+from sqlalchemy.orm import Session
+
+from db.models import Task
+from schema import TaskCreateSchema
+
+
 class TaskService:
     @staticmethod
-    def create_task(task_data):
-        pass
+    def create_task(db: Session, task_data: TaskCreateSchema):
+        db_task = Task(title=task_data.title, description=task_data.description)
+        db.add(db_task)
+        db.commit()
+        db.refresh(db_task)
+        return db_task
