38c0a15391c1b108affd21a362e5e79ceef7604e
Moataz0000 <moatazfawzy150@gmail.com>
2026-03-13 00:39:57 +0200

    feat: created update task endpoint

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 from .db.database import Base, SessionLocal, engine
 from .domain.selector.task import TaskSelector
 from .domain.service.task import TaskService
-from .schema import TaskCreateSchema, TaskRetrieveSchema
+from .schema import TaskCreateSchema, TaskRetrieveSchema, TaskUpdateSchema
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI(
@@ -48,6 +48,19 @@ async def get_task_detail_view(task_id: int, db: Session = Depends(get_db)):
             detail=f"Task with this '{task_id}' is not found!",
         )
     return task_detail
+
+
+@app.put("/tasks/{task_id}/update/", response_model=TaskRetrieveSchema)
+async def update_task_view(
+    task_id: int, task_data: TaskUpdateSchema, db: Session = Depends(get_db)
+):
+    task = TaskService.update_task(db, task_id, task_data)
+    if not task:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Task with this id '{task_id}' is not found!",
+        )
+    return task
 
 
 @app.delete("/tasks/{task_id}/delete/", status_code=status.HTTP_204_NO_CONTENT)
